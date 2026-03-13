772e079df5555c6c2184940555f431eb1e6fc242
Jubayer Nirjus <www.nirjus14@gmail.com>
2026-03-13 15:08:38 +0600

    added python portfolio projects

---
+++ basics/basics/file_read_write.py
@@ -0,0 +1,6 @@
+
+with open("sample.txt","w") as f:
+    f.write("Hello Python")
+
+with open("sample.txt","r") as f:
+    print(f.read())
---
+++ basics/basics/list_dict.py
@@ -0,0 +1,12 @@
+
+numbers = [1,2,3,4,5]
+
+for n in numbers:
+    print(n)
+
+student = {
+    "name":"Jubayer",
+    "age":23
+}
+
+print(student["name"])
