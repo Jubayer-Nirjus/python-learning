fe084d1626922990f4f892b66a8868797edcbcd6
Jubayer Nirjus <www.nirjus14@gmail.com>
2026-03-13 19:53:22 +0600

    message

--- basics/basics/file_read_write.py
+++ basics/basics/file_read_write.py
@@ -3,4 +3,10 @@ with open("sample.txt","w") as f:
     f.write("Hello Python")
 
 with open("sample.txt","r") as f:
-    print(f.read())
+    print(f.read()) 
+
+# Multiplication table
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"{i} x {j} = {i*j}")
+    print("-" * 10) 
